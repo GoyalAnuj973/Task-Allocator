1ad61eda6fdd86eeaafb7bdb0deb2f532e04cde1
anujgoyal4 <anujgoyal4@deloitte.com>
2022-03-02 14:39:11 +0530

    testing deployment

--- app/persons/models.py
+++ app/persons/models.py
@@ -1,7 +1,11 @@
 from statistics import mode
 from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
 
 class Person(models.Model):
     name = models.CharField(max_length=32)
     age = models.IntegerField()
+
+# class User(AbstractUser):
+#     pass
